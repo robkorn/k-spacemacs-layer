4548396df0ce394f1735d0afde7705262e38e11b
Robert Kornacki <11645932+robkorn@users.noreply.github.com>
2018-12-15 15:28:29 -0500

    Layer created

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-k-spacemacs-layer
+K Spacemacs Layer
+=========================
+
+This is a Spacemacs layer for K which provides syntax highlighting via k3-mode.
+
+K3-Mode: https://github.com/kframework/k-editor-support/blob/master/emacs/k3-mode.el
+
+
---
+++ k-framework/packages.el
@@ -0,0 +1,14 @@
+;;; packages.el --- k-framework layer packages file for Spacemacs.
+
+(setq k-framework-packages
+  '(
+    (k3-mode :location local)
+   )
+)
+
+(defun k-framework/init-k3-mode ()
+  (use-package k3-mode
+    :defer t
+    :mode "\\.k\\'"
+  )
+)
